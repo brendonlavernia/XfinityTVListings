079d9d006cc9302983c0782b24f1091e023ac131
Brendon Lavernia <brendonlavernia@gmail.com>
2016-09-19 21:51:33 -0400

    Setup

---
+++ XfinityTVListings/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XfinityTVListings
+//
+//  Created by Brendon Lavernia on 9/19/16.
+//  Copyright © 2016 Comcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
